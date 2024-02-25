9b62a06f77352e218d72d0380b1f988a5a69b9e0
imorochi <imorochi.dev@gmail.com>
2024-02-25 14:48:21 -0400

    17. Implementing Domain Events in Order Service domain layer

---
+++ order-service/order-domain/order-domain-core/src/main/java/bo/food/ordering/system/order/service/domain/OrderDomainService.java
@@ -0,0 +1,18 @@
+package bo.food.ordering.system.order.service.domain;
+
+import bo.food.ordering.system.order.service.domain.entity.Order;
+import bo.food.ordering.system.order.service.domain.entity.Restaurant;
+import bo.food.ordering.system.order.service.domain.event.OrderCancelEvent;
+import bo.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import bo.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+
+import java.util.List;
+
+public interface OrderDomainService {
+
+    OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant);
+    OrderPaidEvent payOrder(Order order);
+    void approveOrder(Order order);
+    OrderCancelEvent cancelOrderPayment(Order order, List<String> failureMessages);
+    void cancelOrder(Order order, List<String> failureMessages);
+}
