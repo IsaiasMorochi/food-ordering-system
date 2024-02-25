d5908500039ee9a479104a21d2c6314a6c68d97f
imorochi <imorochi.dev@gmail.com>
2024-02-25 15:50:48 -0400

    19. Implementing Order Application Service - DTO classes

---
+++ order-service/order-domain/order-application-service/src/main/java/bo/food/ordering/system/order/service/domain/dto/message/RestaurantApprovalResponse.java
@@ -0,0 +1,24 @@
+package bo.food.ordering.system.order.service.domain.dto.message;
+
+import bo.food.ordering.system.domain.vo.OrderApprovalStatus;
+import bo.food.ordering.system.domain.vo.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class RestaurantApprovalResponse {
+    private String id;
+    private String sagaId;
+    private String orderId;
+    private String restaurantId;
+    private Instant createdAt;
+    private OrderApprovalStatus orderApprovalStatus;
+    private List<String> failureMessages;
+}
