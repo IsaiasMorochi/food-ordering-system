8fcbc48c01685c8fb71e18e71456e8349e3c67d8
imorochi <imorochi.dev@gmail.com>
2024-02-25 14:36:50 -0400

    Implementing Domain Events in Order Service domain layer, with refactor duplicate for: OrderCancelEvent, OrderCreatedEvent, OrderPaidEvent

--- order-service/order-domain/order-domain-core/src/main/java/bo/food/ordering/system/order/service/domain/event/OrderCancelEvent.java
+++ order-service/order-domain/order-domain-core/src/main/java/bo/food/ordering/system/order/service/domain/event/OrderCancelEvent.java
@@ -1,24 +1,12 @@
 package bo.food.ordering.system.order.service.domain.event;
 
-import bo.food.ordering.system.domain.event.DomainEvent;
 import bo.food.ordering.system.order.service.domain.entity.Order;
 
 import java.time.ZonedDateTime;
 
-public class OrderCancelEvent implements DomainEvent<Order> {
-    private final Order order;
-    private final ZonedDateTime createdAt;
+public class OrderCancelEvent extends OrderEvent {
 
     public OrderCancelEvent(Order order, ZonedDateTime createdAt) {
-        this.order = order;
-        this.createdAt = createdAt;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public ZonedDateTime getCreatedAt() {
-        return createdAt;
+        super(order, createdAt);
     }
 }
