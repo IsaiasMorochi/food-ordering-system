50286663d8aee6e0e0d4d52bff0737a0f755351a
imorochi <imorochi.dev@gmail.com>
2024-02-26 18:40:02 -0400

    21. Implementing input ports in Order Application Service

---
+++ order-service/order-domain/order-application-service/src/main/java/bo/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -0,0 +1,15 @@
+package bo.food.ordering.system.order.service.domain;
+
+import bo.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import bo.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OrderTrackCommandHandler {
+
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+
+    }
+}
