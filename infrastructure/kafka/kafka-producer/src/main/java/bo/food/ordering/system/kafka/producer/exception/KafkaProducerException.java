6e656f5748e867dee8db98fecd1dff6de5da0f98
imorochi <imorochi.dev@gmail.com>
2024-03-16 21:09:59 -0400

    30. Implementing kafka producer generic module

---
+++ infrastructure/kafka/kafka-producer/src/main/java/bo/food/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package bo.food.ordering.system.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
