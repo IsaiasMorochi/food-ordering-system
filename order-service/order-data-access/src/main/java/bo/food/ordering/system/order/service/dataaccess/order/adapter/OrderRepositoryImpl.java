17a2fd803621f50afa0078d61b124e4b87496d38
imorochi <imorochi.dev@gmail.com>
2024-03-26 21:09:06 -0400

    33. Data access module: Adding JPA Repository & Adapter implementation

---
+++ order-service/order-data-access/src/main/java/bo/food/ordering/system/order/service/dataaccess/order/adapter/OrderRepositoryImpl.java
@@ -0,0 +1,37 @@
+package bo.food.ordering.system.order.service.dataaccess.order.adapter;
+
+import bo.food.ordering.system.domain.vo.OrderId;
+import bo.food.ordering.system.order.service.domain.vo.TrackingId;
+import bo.food.ordering.system.order.service.dataaccess.order.mapper.OrderDataAccessMapper;
+import bo.food.ordering.system.order.service.dataaccess.order.repository.OrderJpaRepository;
+import bo.food.ordering.system.order.service.domain.entity.Order;
+import bo.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class OrderRepositoryImpl implements OrderRepository {
+
+    private final OrderJpaRepository orderJpaRepository;
+    private final OrderDataAccessMapper orderDataAccessMapper;
+
+    public OrderRepositoryImpl(OrderJpaRepository orderJpaRepository,
+                               OrderDataAccessMapper orderDataAccessMapper) {
+        this.orderJpaRepository = orderJpaRepository;
+        this.orderDataAccessMapper = orderDataAccessMapper;
+    }
+
+    @Override
+    public Order save(Order order) {
+        return orderDataAccessMapper.orderEntityToOrder(orderJpaRepository
+                .save(orderDataAccessMapper.orderToOrderEntity(order)));
+    }
+
+    @Override
+    public Optional<Order> findByTrackingId(TrackingId trackingId) {
+        return orderJpaRepository.findByTrackingId(trackingId.getValue())
+                .map(orderDataAccessMapper::orderEntityToOrder);
+    }
+
+}
--- order-service/order-domain/order-application-service/src/main/java/bo/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
+++ order-service/order-domain/order-application-service/src/main/java/bo/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
@@ -9,4 +9,5 @@ public interface OrderRepository {
 
     Order save(Order order);
     Optional<Order> findByTrackingId(TrackingId trackingId);
+
 }
