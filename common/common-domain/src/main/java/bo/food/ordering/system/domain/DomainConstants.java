35478a8b05f01167507c9c866dbbe0367547e65f
imorochi <imorochi.dev@gmail.com>
2024-04-07 11:57:34 -0400

    46. Domain core module: Implementing Payment Domain

---
+++ common/common-domain/src/main/java/bo/food/ordering/system/domain/DomainConstants.java
@@ -0,0 +1,9 @@
+package bo.food.ordering.system.domain;
+
+public class DomainConstants {
+
+    private DomainConstants() {
+    }
+
+    public static final String UTC = "UTC";
+}
---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/bo/food/ordering/system/payment/service/domain/PaymentDomainService.java
@@ -0,0 +1,21 @@
+package bo.food.ordering.system.payment.service.domain;
+
+import bo.food.ordering.system.payment.service.domain.entity.CreditEntry;
+import bo.food.ordering.system.payment.service.domain.entity.CreditHistory;
+import bo.food.ordering.system.payment.service.domain.entity.Payment;
+import bo.food.ordering.system.payment.service.domain.event.PaymentEvent;
+
+import java.util.List;
+
+public interface PaymentDomainService {
+
+    PaymentEvent validateAndInitiatePayment(Payment payment,
+                                            CreditEntry creditEntry,
+                                            List<CreditHistory> creditHistories,
+                                            List<String> failureMessages);
+
+    PaymentEvent validateAndCancelPayment(Payment payment,
+                                          CreditEntry creditEntry,
+                                          List<CreditHistory> creditHistories,
+                                          List<String> failureMessages);
+}
