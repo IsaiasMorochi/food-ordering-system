6da6799d76aaacc2beb20608882891d7dd1ce21b
imorochi <imorochi.dev@gmail.com>
2024-05-19 17:45:03 -0400

    57. Application Service domain module Adding Mapper, DTO and ports

---
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/bo/food/ordering/system/restaurant/service/domain/ports/output/repository/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package bo.food.ordering.system.restaurant.service.domain.ports.output.repository;
+
+import bo.food.ordering.system.restaurant.service.domain.entity.Restaurant;
+
+import java.util.Optional;
+
+public interface RestaurantRepository {
+    Optional<Restaurant> findRestaurantInformation(Restaurant restaurant);
+
+}
